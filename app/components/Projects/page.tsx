be7b098cd771be5e7501baf4023d069b9437f1a3
Niavo <129644369+RaNiiJohnson@users.noreply.github.com>
2024-01-19 18:48:18 +0300

    Update page.tsx

--- app/components/Projects/page.tsx
+++ app/components/Projects/page.tsx
@@ -1,4 +1,4 @@
-import { Code2 } from "lucide-react";
+import { Code2, Eye } from "lucide-react";
 import Link from "next/link";
 
 export default function Projects() {
@@ -9,7 +9,7 @@ export default function Projects() {
       </h1>
       <div className="bg-slate-800 flex  sm:flex-row justify-between items-center mt-4 p-4 rounded-lg">
         <section>
-          <h1 className="font-semibold sm:text-xl mb-1 text-base">
+          <h1 className="font-semibold text-xl mb-1">
             BLOG-NOTE
             <span className="font-thin text-xs opacity-80 ">(2023)</span>
           </h1>
@@ -37,24 +37,22 @@ export default function Projects() {
         </section>
         <Link
           href="https://github.com/RaNiiJohnson/MERN-Blog-Note"
-          target="_blank"
-          className="bg-slate-700 sm:py-2 sm:px-4 py-1 px-2 rounded-lg cursor-pointer hover:bg-blue-600 transition-all"
+          className="bg-slate-700 py-2 px-4 rounded-lg cursor-pointer hover:bg-blue-600 transition-all"
         >
           <Code2 />
         </Link>
       </div>
       <div className="bg-slate-800 flex  sm:flex-row justify-between items-center mt-4 p-4 rounded-lg">
         <section>
-          <h1 className="font-semibold sm:text-xl mb-1 text-base">
+          <h1 className="font-semibold text-xl mb-1">
             PostYeaah
             <span className="font-thin text-xs opacity-80">(2023)</span>
           </h1>
           <div className="text-xs opacity-65 mb-4 mt-1 max-w-[500px] ml-1 sm:block hidden">
             un espace en ligne dedié pour les publication sans images ni
-            mentions &quot; jaime &quot;, favorisant en revanche les
-            commentaires.
+            mentions " j'aime ", favorisant en revanche les commentaires.
           </div>
-          <div className="flex flex-row flex-wrap sm:gap-5 gap-3">
+          <div className="flex flex-row flex-wrap gap-5">
             <div className="text-xs flex flex-row items-center gap-1">
               <span className="h-2 w-2 bg-blue-400 rounded-full"></span> React
             </div>
@@ -73,22 +71,21 @@ export default function Projects() {
         </section>
         <Link
           href="https://github.com/RaNiiJohnson/MERN-PostYeaah"
-          target="_blank"
-          className="bg-slate-700  sm:py-2 sm:px-4 py-1 px-2 ml-1 rounded-lg cursor-pointer hover:bg-blue-600 transition-all"
+          className="bg-slate-700  py-2 px-4 rounded-lg cursor-pointer hover:bg-blue-600 transition-all"
         >
           <Code2 />
         </Link>
       </div>
       <div className="bg-slate-800 flex  sm:flex-row justify-between items-center mt-4 p-4 rounded-lg">
         <section>
-          <h1 className="font-semibold sm:text-xl mb-1 text-base">
+          <h1 className="font-semibold text-xl mb-1">
             Threads-clone-yeah
             <span className="font-thin text-xs opacity-80 ">(2024)</span>
           </h1>
           <div className="text-xs opacity-65 mb-4 mt-1 max-w-[500px] ml-1 sm:block hidden">
             Clone de Threads
           </div>
-          <div className="flex flex-row flex-wrap sm:gap-5 gap-3">
+          <div className="flex flex-row flex-wrap gap-5">
             <div className="text-xs flex flex-row items-center gap-1">
               <span className="h-2 w-2 bg-slate-300 rounded-full"></span>
               Next.js
@@ -111,13 +108,20 @@ export default function Projects() {
             </div>
           </div>
         </section>
-        <Link
-          href="https://github.com/RaNiiJohnson/threads-clone-yeah"
-          target="_blank"
-          className="bg-slate-700 sm:py-2 sm:px-4 py-1 px-2 rounded-lg cursor-pointer hover:bg-blue-600 transition-all"
-        >
-          <Code2 />
-        </Link>
+        <div className="flex flex-col gap-2">
+          <Link
+            href="https://github.com/RaNiiJohnson/threads-clone-yeah"
+            className="bg-slate-700  py-2 px-4 rounded-lg cursor-pointer hover:bg-blue-600 transition-all"
+          >
+            <Code2 />
+          </Link>
+          <Link
+            href="https://threads-clone-yeah.vercel.app/"
+            className="bg-slate-700  py-2 px-4 rounded-lg cursor-pointer hover:bg-orange-700 transition-all"
+          >
+            <Eye />
+          </Link>
+        </div>
       </div>
     </div>
   );
