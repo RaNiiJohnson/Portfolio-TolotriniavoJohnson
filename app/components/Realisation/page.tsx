7dfcd4b64325e33c4246931becbb68daa766bd6c
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-03-09 22:43:13 +0300

    chore : code optimization

--- app/components/Realisation/page.tsx
+++ app/components/Realisation/page.tsx
@@ -1,4 +1,20 @@
+import { LastSection, Section } from "./Section";
+
 export default function Realisation() {
+  const mois = [
+    "Jan.",
+    "Fev.",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juil.",
+    "Aout",
+    "Sept.",
+    "Oct.",
+    "Nov.",
+    "Dec.",
+  ];
   return (
     <div>
       <h1 id="chronologie" className="text-xl font-bold uppercase sm:text-3xl">
@@ -7,108 +23,37 @@ export default function Realisation() {
       <div className="overflow-x-auto ">
         <div className="w-[850px] flex gap-5 relative  border mt-4 border-solid border-opacity-20 border-stone-300 px-4 py-1 rounded-md">
           <div className=" w-[800px] grid grid-cols-12 pb-3">
-            {[
-              "Jan.",
-              "Fev.",
-              "Mars",
-              "Avril",
-              "Mai",
-              "Juin",
-              "Juil.",
-              "Aout",
-              "Sept.",
-              "Oct.",
-              "Nov.",
-              "Dec.",
-            ].map((mois, index) => (
+            {mois.map((mois, index) => (
               <section key={index} className="col-span-1">
                 {mois}
               </section>
             ))}
             {/* 2023 -------------------------------------------------------------------------*/}
             {/* BLOG-NOTE */}
-            <div className="col-span-3">
-              <div className="grid grid-cols-12">
-                {Array.from({ length: 2 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-800"
-                  ></section>
-                ))}
-                {Array.from({ length: 6 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-500"
-                  ></section>
-                ))}
-                {Array.from({ length: 4 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-purple-500"
-                  ></section>
-                ))}
-              </div>
-            </div>
-            <div className="col-span-3">
-              <div className="grid grid-cols-12">
-                {Array.from({ length: 4 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-500"
-                  ></section>
-                ))}
-                {Array.from({ length: 5 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-purple-500"
-                  ></section>
-                ))}
-                {Array.from({ length: 3 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-500"
-                  ></section>
-                ))}
-              </div>
-            </div>
-            <div className="col-span-3">
-              <div className="grid grid-cols-12">
-                {Array.from({ length: 4 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-500"
-                  ></section>
-                ))}
-                {Array.from({ length: 2 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-800"
-                  ></section>
-                ))}
-                {Array.from({ length: 6 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-500"
-                  ></section>
-                ))}
-              </div>
-            </div>
-            <div className="col-span-3">
-              <div className="grid grid-cols-12">
-                {Array.from({ length: 10 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-500"
-                  ></section>
-                ))}
-                {Array.from({ length: 2 }).map((_, index: number) => (
-                  <section
-                    key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-yellow-400"
-                  ></section>
-                ))}
-              </div>
-            </div>
+            <Section
+              firstClassName="col-span-1 mb-1 size-3 bg-slate-500"
+              SecondClassName="col-span-1 mb-1 bg-purple-500 size-3"
+              firstLength={8}
+              secondLength={4}
+            />
+            <Section
+              firstClassName="col-span-1 mb-1 bg-purple-500 size-3"
+              SecondClassName="col-span-1 mb-1 size-3 bg-slate-500"
+              firstLength={5}
+              secondLength={7}
+            />
+            <Section
+              firstClassName="col-span-1 mb-1 size-3 bg-slate-500"
+              SecondClassName="col-span-1 mb-1 size-3 bg-slate-500"
+              firstLength={4}
+              secondLength={8}
+            />
+            <Section
+              firstClassName="col-span-1 mb-1 size-3 bg-slate-500"
+              SecondClassName="col-span-1 mb-1 bg-yellow-400 size-3"
+              firstLength={10}
+              secondLength={2}
+            />
 
             {/* 2024 ------------------------------------------------------------------------*/}
             <div className="col-span-3">
@@ -116,25 +61,31 @@ export default function Realisation() {
                 {Array.from({ length: 3 }).map((_, index: number) => (
                   <section
                     key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-yellow-400"
+                    className="col-span-1 mb-1 bg-yellow-400 size-3"
                   ></section>
                 ))}
                 {Array.from({ length: 1 }).map((_, index: number) => (
                   <section
                     key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-500"
+                    className="col-span-1 mb-1 size-3 bg-slate-500"
                   ></section>
                 ))}
                 {Array.from({ length: 3 }).map((_, index: number) => (
                   <section
                     key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-blue-600"
+                    className="col-span-1 mb-1 bg-blue-600 size-3"
                   ></section>
                 ))}
-                {Array.from({ length: 5 }).map((_, index: number) => (
+                {Array.from({ length: 2 }).map((_, index: number) => (
                   <section
                     key={index}
-                    className="w-3 h-3 col-span-1 mb-1 bg-slate-800"
+                    className="col-span-1 mb-1 size-3 bg-slate-500"
+                  ></section>
+                ))}
+                {Array.from({ length: 3 }).map((_, index: number) => (
+                  <section
+                    key={index}
+                    className="col-span-1 mb-1 size-3 bg-slate-800"
                   ></section>
                 ))}
               </div>
@@ -145,7 +96,7 @@ export default function Realisation() {
                   {Array.from({ length: 12 }).map((_, index: number) => (
                     <section
                       key={index}
-                      className="w-3 h-3 col-span-1 mb-1 bg-slate-800"
+                      className="col-span-1 mb-1 size-3 bg-slate-800"
                     ></section>
                   ))}
                 </div>
@@ -158,23 +109,23 @@ export default function Realisation() {
           </div>
         </div>
       </div>
-      <div className="my-4 text-sm">
-        <div className="flex items-center gap-2 text-sm">
-          <section className="w-2 h-2 bg-purple-500 rounded-lg sm:h-4 sm:w-6"></section>
-          BLOG-NOTE
-        </div>
-        <div className="flex items-center gap-2 text-sm">
-          <section className="w-2 h-2 bg-yellow-400 rounded-full sm:h-4 sm:w-6"></section>
-          Threads-clone-yeah
-        </div>
-        <div className="flex items-center gap-2 text-sm">
-          <section className="w-2 h-2 bg-blue-600 rounded-lg sm:h-4 sm:w-6"></section>
-          DevDialogue
-        </div>
-        <div className="flex items-center gap-2 text-sm">
-          <section className="w-2 h-2 rounded-lg bg-slate-500 sm:h-4 sm:w-6"></section>
-          Projets variés et formation continue.
-        </div>
+      <div className="mt-4 space-y-1 text-sm">
+        <LastSection
+          className="bg-purple-500 rounded-lg size-2 sm:h-4 sm:w-6"
+          name="BLOG-NOTE"
+        />
+        <LastSection
+          className="bg-yellow-400 rounded-lg size-2 sm:h-4 sm:w-6"
+          name="Threads-clone-yeah"
+        />
+        <LastSection
+          className="bg-blue-600 rounded-lg size-2 sm:h-4 sm:w-6"
+          name="DevDialogue"
+        />
+        <LastSection
+          className="rounded-lg size-2 bg-slate-500 sm:h-4 sm:w-6"
+          name="Projets variés et formation continue."
+        />
       </div>
     </div>
   );
