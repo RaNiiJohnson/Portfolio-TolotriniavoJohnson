1c38743230f7f682d0ef8c0405ab52b6a472db17
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-03-09 22:41:35 +0300

    my portfolio

--- app/components/Presentation/page.tsx
+++ app/components/Presentation/page.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import Image from "next/image";
 import Projects from "../Projects/page";
 import Realisation from "../Realisation/page";
-import Image from "next/image";
 import nextjs from "../assets/Next.js.svg";
 import express from "../assets/express.png";
 import mongodb from "../assets/mongodb.png";
@@ -14,14 +13,14 @@ export default function Presenation() {
     <>
       <div
         id="presentaion"
-        className=" border border-solid border-opacity-20 border-stone-300 rounded-md "
+        className="border border-solid rounded-md border-opacity-20 border-stone-300"
       >
         <section className="">
-          <div className="bg-slate-800 px-4 pt-3 pb-1 text-sm">
+          <div className="px-4 pt-3 pb-1 text-sm bg-slate-800">
             Johnson Tolotriniavo / ReadMe.
             <span className="text-xs opacity-60">md</span>
           </div>
-          <h1 className="font-bold px-4 py-3 sm:text-3xl text-xl">
+          <h1 className="px-4 py-3 text-xl font-bold sm:text-3xl">
             BIENVENUE SUR <span className="text-blue-700">MON PORTFOLIO</span>
           </h1>
           <p className="px-4">
@@ -29,34 +28,34 @@ export default function Presenation() {
             l&apos;apprentissage du code, depuis 2021, je me forme à temps plein
             en autodidacte en tant que développeur Javascript et ReactJS afin de
             devenir le plus rapidement possible développeur. Plus qu&apos;un
-            avenir proffessionel, je me suis découvert une passion, celle de
+            avenir professionnel, je me suis découvert une passion, celle de
             coder,
           </p>
         </section>
         <section className="px-4 py-6">
-          <h1 className="font-bold sm:text-3xl text-xl">STACK</h1>
+          <h1 className="text-xl font-bold sm:text-3xl">STACK</h1>
           <div className="flex flex-wrap mt-2">
-            <span className="bg-slate-800 flex items-center gap-1 py-2 px-4 mr-4 my-2 rounded-md">
+            <span className="flex items-center gap-1 px-4 py-2 my-2 mr-4 rounded-md bg-slate-800">
               <Image src={reactjs} alt="" className="w-6" />
               ReactJS
             </span>
-            <span className="bg-slate-800 flex items-center py-2 pr-4 pl-1 mr-4 my-2 rounded-md">
+            <span className="flex items-center py-2 pl-1 pr-4 my-2 mr-4 rounded-md bg-slate-800">
               <Image src={nextjs} alt="" className="w-11" />
               NextJS
             </span>
-            <span className="bg-slate-800 flex items-center gap-1 py-2 px-4 mr-4 my-2 rounded-md">
+            <span className="flex items-center gap-1 px-4 py-2 my-2 mr-4 rounded-md bg-slate-800">
               <Image src={nodejs} alt="" className="w-6" />
               NodeJS
             </span>
-            <span className="bg-slate-800 flex items-center gap-1 py-2 px-4 mr-4 my-2 rounded-md">
+            <span className="flex items-center gap-1 px-4 py-2 my-2 mr-4 rounded-md  bg-slate-800">
               <Image src={tailwind} alt="" className="w-6" />
               TailwindCSS
             </span>
-            <span className="bg-slate-800 flex items-center gap-1 py-2 px-4 mr-4 my-2 rounded-md">
+            <span className="flex items-center gap-1 px-4 py-2 my-2 mr-4 rounded-md bg-slate-800">
               <Image src={express} alt="" className="w-6" />
               ExpressJS
             </span>
-            <span className="bg-slate-800 flex items-center gap-1 py-2 px-4 mr-4 my-2 rounded-md">
+            <span className="flex items-center gap-1 px-4 py-2 my-2 mr-4 rounded-md bg-slate-800">
               <Image src={mongodb} alt="" className="w-6" />
               MongoDB
             </span>
